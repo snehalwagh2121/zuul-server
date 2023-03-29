18112461bc16d5d7eb64cccb44b79d28e4f7cc23
Snehal <snehalwagh2121@gmail.com>
2023-03-29 09:36:35 +0530

    SPRING-38 - Gateway service changes

--- src/main/java/com/example/zuulserver/ZuulServerApplication.java
+++ src/main/java/com/example/zuulserver/ZuulServerApplication.java
@@ -3,12 +3,8 @@ package com.example.zuulserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 @SpringBootApplication
-@EnableZuulProxy
-@EnableZuulServer
 @EnableDiscoveryClient
 public class ZuulServerApplication {
 
